2f1d82f464be3fde6f8006fcfd292798b3161b0d
AnliHuer <AnliHuer@outlook.com>
2015-12-11 15:18:39 +0800

    views: use bootstrap modal to complete the function addModal

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,6 @@
 $(function(){
-  var str = $('body').html();
-  console.log(str);
-  $.get('/index', function(resp) {
-    console.log(1111111111);
-    console.log(resp);
+
+    $('#addModal').modal(options);
+
   });
 });
